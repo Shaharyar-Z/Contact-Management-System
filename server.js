d868fe3805e85750ba3656346447ba03590040f1
shaharyar-zaheer <83276819+Shaharyar-Z@users.noreply.github.com>
2021-10-01 17:22:44 +0500

    Added some features

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const connectDb = require('./config/db')
+const connectDb = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,15 +8,21 @@ const app = express();
 connectDb();
 
 // Init middleware
-app.use(express.json({ extended: false}))
-
-app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Manager API' }))
+app.use(express.json({ extended: false }));
 
 // Routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+
+// Server static files on production server
+if(process.env.NODE_ENV === 'production'){
+    // Set Static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT =process.env.PORT || 5000;
 
-app.listen(PORT,() => console.log(`Server starting on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server starting on PORT ${PORT}`));
